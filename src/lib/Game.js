81ba59056d0f54434ec318a93e960067d31650dc
Sam Foster <sam@sam-i-am.com>
2011-06-19 03:37:22 +0100

    Sketching out architecture using Rosewood lib. qtower is our 'main' and configures and bootstraps the Game instance

--- src/lib/Game.js
+++ src/lib/Game.js
@@ -1,18 +1,53 @@
 define([
+	'lib/rosewood', 
 	'lib/my-class/my.class', 
 	'lib/base',
 	'lib/lang'
-], function (my, base, lang){
+], function (rw, my, base, lang){
 
 	var Game = my.Class(base.Game, {
 		config: null,
+		sprites: null,
 		constructor: function(props) {
 			Game.Super.call(this, props);
+			this.sprites = {};
 		},
 		setup: function(){
+			var sprites = this.sprites, 
+				config = this.config, 
+				modules = Array.prototype.slice.call(arguments);
+			console.log("modules: ", modules);
+			modules.forEach(function(mod){
+				if(mod.sprites) {
+					for(var i in mod.sprites) {
+						sprites[i] = mod.sprites[i];
+						console.log("setup sprite in module: " + i);
+						// populate any templated paths with our config
+						if(sprites[i][0].indexOf('${') > -1){
+							sprites[i][0] = lang.templatize(sprites[i][0], config);
+						}
+					}
+				}
+			});
+			
+			console.log("setup sprites: ", sprites);
 			this._setupMenu();
 			this._setupMap();
 			this._setupPlayer();
+			
+			rw.loadSprites(this.sprites, lang.bind(this, function() {
+				engine = rw.init('playground', {
+					x:this.width,
+					y:this.height,
+					FPS:40,
+					sequence:['ents','blit'],
+					keys:['ua','da','la','ra']
+				});
+				this.onReady();
+				console.log("rw init and newEnt called");
+			}))
+
+			
 		},
 		_setupMap: function(){
 			// need a node/context to render to
--- src/lib/qtower.js
+++ src/lib/qtower.js
@@ -1,8 +1,13 @@
-define(['lib/rosewood', "lib/lang", "lib/config", "lib/Game"], function (
+define([
+		'lib/rosewood', "lib/lang", "lib/config", "lib/Game",
+		'lib/npc', 'lib/player'
+	], function (
 		rw,  // Rosewood module
 		lang, 
-		config, 
-		Game
+		baseConfig, 
+		Game,
+		npc,
+		player
 	){
 	// bootstrap for the qtower game
 	// TODO: define concete Game instance here
@@ -10,34 +15,39 @@ define(['lib/rosewood', "lib/lang", "lib/config", "lib/Game"], function (
 	
 	qtower.bootstrap = function(config) {
 		// Initialize the game:
-		console.log("get new Game");
-
 		// game logic is contained in the Game class
+		// 	stuff like setting up each round/level, menu population etc.
+		var mixin = lang.mixin, 
+			myConfig = mixin(mixin({}, baseConfig), config);
+		
 		var game = qtower.game = new Game({
 			id: "qtower",
 			stageNode: config.stageNode,
 			height: config.stageNode.offsetHeight,
 			width: config.stageNode.offsetWidth,
-			config: config,
+			config: myConfig,
 			keyTracker: true
 		});
 		console.log("/get new Game");
 		qtower.start = qtower.game.start;
 		
-		game.setup();
-		var assetsDir = game.config.assetsDir;
+		game.onReady = function() {
+			console.log("onReady");
+			var e1 = window.e1 = new npc.Enemy("enemy1");
+			rw.newEnt( e1 )
+				.base.display(234, 0, 234)
+				.end();
 
-		var Enemy = function(name) {
-			console.log("in Enemy ctor ", rw, rw.Ent);
-			// nameIn, spriteIn, widthIn, heightIn
-			this.base = new rw.Ent(name, 'e1', 50, 50);
-			this.update = function(){
-				if (rw.key('ua')) this.base.move(0,-2);
-				if (rw.key('da')) this.base.move(0,2);
-				if (rw.key('la')) this.base.move(-2,0);
-				if (rw.key('ra')) this.base.move(2,0);
-			}
+			var t1 = window.t1 = new player.Tower("tower1");
+			rw.newEnt( t1 )
+				.base.display(234, 234, 234)
+				.end();
 		}
+		game.setup(
+			npc, player
+			// line up all the pieces
+		);
+
 		// sequence
 		// build map (at level x)
 		// init player w. heath, points, credits etc.
@@ -47,27 +57,15 @@ define(['lib/rosewood', "lib/lang", "lib/config", "lib/Game"], function (
 		//	shop: the menu bit where you pick what towers to deploy
 
 		//initialize the start button
-		// $("#startbutton").click(function(){
-		// 	console.log("on startbutton click");
-		// })
-
-		rw.loadSprites({
-			'e1': [assetsDir + '/enemy1.png', 50, 50, 0, 0]
-		}, function() {
-			var game = rw.init('playground', {
-				x:500,
-				y:500,
-				FPS:40,
-				sequence:['ents','blit'],
-				keys:['ua','da','la','ra']
-			});
-			var e1 = window.e1 = new Enemy("enemy1") ;
-			game.newEnt( e1 )
-				.base.display(234, 234, 234)
-				.end()
-			.start();
-			console.log("rw init and newEnt called");
-		})
+		var engine;
+		qtower.start = function(){
+			rw.start();
+		}; 
+		qtower.stop = function(){
+			rw.stop();
+		}; 
+		$("#startBtn").click(qtower.start);
+		$("#stopBtn").click(qtower.stop);
 
 	};
 	
