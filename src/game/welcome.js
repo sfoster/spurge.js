c6f2925606c8484fad3e5291080d5e439b35eac2
Sam Foster <sam@sam-i-am.com>
2011-07-10 11:21:07 +0100

    More scene hookup

--- src/game/welcome.js
+++ src/game/welcome.js
@@ -1,31 +1,24 @@
 define([
 		'lib/lang',
 		'lib/Compose',
-		'lib/Scene'
+		'game/Scene'
 	], function (lang, Compose, Scene){
-	
-	var scene = new Compose(function(){
-		console.log("the welcome scene ctor");
-		this.ctorfoo = "ctorbar";
-	}, Scene, 
+
+	var after = Compose.after, 
+		before = Compose.before, 
+		from = Compose.from;
+
+	return Compose.create(function(){
+		console.log("welcome ctor");
+	},Scene, 
 	{
 		// update: notimpl("update"),
 		// redraw: notimpl("redraw"),
-		foo: "bar",
-		enter: function(){
-			console.log("Welcome Scene entered:", node);
-			var node = this.config.get("gameNode"); 
-			node.appendChild( document.createTextNode("Welcome Scene entered") );
-		},
-		exit: function(){
-			console.log("Welcome Scene exit");
-		},
-		load: function(){
-			console.log("Welcome Scene loading");
-		},
-		unload: function(){
-			console.log("Scene unloading");
-		},
+		id: "welcome",
+		className: "scene scene-welcome",
+		render: from(Scene),
+		exit: from(Scene),
+		load: from(Scene),
+		unload: from(Scene),
 	});
-	return scene;
 });
