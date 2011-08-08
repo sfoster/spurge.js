45936846f8fa212e019f06f308112913cb1df582
Sam Foster <sam@sam-i-am.com>
2011-08-08 01:18:38 +0100

    Implementing onHit handling for the Actor; move to remove overlap+2px, and play the glow effect

--- src/lib/entity.js
+++ src/lib/entity.js
@@ -81,10 +81,91 @@ define([
 			delete registry[this.id];
 			
 		},
-		onHit: function(/*hitee*/entity){
+		onHit: function(/*hitee*/ent){
+			// we respond differently if we get hit by a missile vs. bumping into a wall
+			// TODO: refactor to allow handling for different collision types
+			// maybe test for onHitBy{type} method otherwise use default handling
+			
 			// console.log(this.id +": got hit");
-			// reset (we can get hit while this effect is tailing off)
+
+			// reset effects in ms
 			var hitDecay = 64;
+
+			
+			// should entities have velocity property
+			// which we could use here to determine from which direction 
+			// we were hit?
+			// for now, we'll assume the edge where we overlap 
+			// is the direction the hit came from
+			
+			// 
+			var p1 = {
+					x: this.x+(this.width/2),
+					y: this.y+(this.height/2),
+					r: Math.max(this.width, this.height)/2
+				}; 
+
+			var p2 = {
+					x: ent.x+(ent.width/2),
+					y: ent.y+(ent.height/2),
+					r: Math.max(ent.width, ent.height)/2
+				}; 
+				
+			// find the angle btw. 2 points
+			// and the nearest edge it corresponds to
+			var rad = Math.atan2(p2.y - p1.y, p2.x - p1.x);
+			// express radians as 1-8 to give us the edge/corner
+			var edge = Math.round(8 * (Math.PI+rad) / (Math.PI+Math.PI));
+			
+				
+			// console.log("x/y: %s, angle: %s, edge: %s", this.x+"/"+this.y, rad, edge);
+			// I'm sure there's a clever way to do this without the switch
+			// maybe rotate and bitmask?
+			switch(edge) {
+				case 2: // North edge collision
+					// top edge collision
+					this.y = ent.y + ent.height + 2; // back off a bit
+					this.dirty('y'); // next redraw will move us
+					break;
+				case 3: //  NE collision
+					this.y = ent.y + ent.height + 2; // back off a bit
+					this.x = ent.x - this.width - 2; // back off a bit
+					this.dirty('y'); // next redraw will move us
+					this.dirty('x'); // next redraw will move us
+					break;
+				case 4: //  E collision
+					this.x = ent.x - this.width - 2; // back off a bit
+					this.dirty('x'); // next redraw will move us
+					break;
+				case 5: //  SE collision
+					this.y = ent.y - this.height - 2; // back off a bit
+					this.x = ent.x - this.width - 2; // back off a bit
+					this.dirty('x'); // next redraw will move us
+					this.dirty('y'); // next redraw will move us
+					break;
+				case 6: //  S collision
+					this.y = ent.y - this.height - 2; // back off a bit
+					this.dirty('y'); // next redraw will move us
+					break;
+				case 7: //  SW collision
+					this.y = ent.y - this.height - 2; // back off a bit
+					this.x = ent.x + ent.width + 2; // back off a bit
+					this.dirty('x'); // next redraw will move us
+					this.dirty('y'); // next redraw will move us
+					break;
+				case 8: //  W collision
+				case 0: //  W collision
+					this.x = ent.x + ent.width + 2; // back off a bit
+					this.dirty('x'); // next redraw will move us
+					break;
+				default: //  NW collision
+					this.x = ent.x + ent.width + 2; // back off a bit
+					this.y = ent.y + ent.height + 2; // back off a bit
+					this.dirty('x'); // next redraw will move us
+					this.dirty('y'); // next redraw will move us
+					break;
+			}
+			// console.log("adjust to x/y: %s", this.x+"/"+this.y);
 
 			// we could apply different behavior depending on what we collided with
 			this.glow = hitDecay;
