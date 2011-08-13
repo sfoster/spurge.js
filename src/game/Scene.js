951552344dc914df41d1013dbee683becfec6367
Sam Foster <sam@sam-i-am.com>
2011-08-13 01:04:24 +0100

    Breaking out the key controls into its own module

--- src/game/Scene.js
+++ src/game/Scene.js
@@ -1,55 +1,17 @@
 define([
 		'lib/lang',
 		'lib/Compose',
-		'lib/Evented',
+		'lib/controls',
 		'lib/Scene',
 		'game/config'
-	], function (lang, Compose, Evented, Scene, config){
+	], function (lang, Compose, controls, Scene, config){
 	
 	var after = Compose.after, 
 		before = Compose.before, 
 		from = Compose.from;
 
-	// TODO: move this keys manager elsewhere
-	var keysMap = {
-		13: "ENTER",
-		16: "SHIFT",
-		' ': "SPACE",
-		32: "SPACE",
-		37: "LEFTARROW",
-		38: "UPARROW",
-		39: "RIGHTARROW",
-		40: "DOWNARROW"
-	};
-	var controls = Compose.create(function(){
-		this.keys ={};
-	}, {
-		keyPressDown: function(evt){
-			var code = evt.keyCode, 
-				chr = (code in keysMap) ? 
-					keysMap[code] : String.fromCharCode(code).toLowerCase() || code;
-					
-			// fallback to the keyCode for non-printable chars e.g. ENTER (13)
-			this.keys[chr] = true;
-			// this.raiseEvent("/keys/down/"+evt.keyCode);
-		},
-		keyPressUp: function(evt){
-			var code = evt.keyCode, 
-				chr = (code in keysMap) ? 
-					keysMap[code] : String.fromCharCode(code).toLowerCase() || code;
-					
-			// use the keyCode for non-printable chars e.g. ENTER (13)
-			delete this.keys[chr || code];
-			// this.raiseEvent("/keys/up/"+evt.keyCode);
-		},
-		init: function(container){
-			console.log("controls init");
-			container = container || document;
-			container.addEventListener('keydown',lang.bind(this, "keyPressDown"),true);
-			container.addEventListener('keyup',lang.bind(this, "keyPressUp"),true);
-		}
-	}, Evented);
-
+  
+	var controls = new controls.Controls();
 	var hdl = controls.addEventListener("/keys/down/ENTER", function(){
 		console.info("ENTER was pressed");
 	});
